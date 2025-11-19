cda918f139267ffd6f530a869c4059027444ee91
Vladislav Vagner <vladislawwagner@gmail.com>
2025-11-19 06:50:33 +0100

    added docker prod

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,16 @@ import { ValidationPipe } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({
-    origin: ["http://localhost:4200"],
-    credentials: true,
-    methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type,Authorization",
-  });
-  await app.listen(process.env.PORT ?? 3000);
+
+  if (process.env.NODE_ENV !== "production") {
+    app.enableCors({
+      origin: ["http://localhost:4200"],
+      credentials: true,
+      methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+      allowedHeaders: "Content-Type,Authorization",
+    });
+  }
+
+  await app.listen(3000);
 }
 bootstrap();
